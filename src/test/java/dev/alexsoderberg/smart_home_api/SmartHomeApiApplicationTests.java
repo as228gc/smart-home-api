7d6f6d9b2209c8cd38a2a67c9dcfe0579da690d8
Alex Söderberg <as228gc@student.lnu.se>
2024-12-04 15:18:28 +0100

    Initialize Spring Boot project

---
+++ src/test/java/dev/alexsoderberg/smart_home_api/SmartHomeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alexsoderberg.smart_home_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
